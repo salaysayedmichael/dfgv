03c49ea6856ab13217c4bc93ef722aa0151823dc
wenman <itsmedaveomandam@gmail.com>
2019-03-13 13:09:39 +0800

    Already added login and dashboard

---
+++ application/controllers/main.controller.php
@@ -0,0 +1,38 @@
+<?php
+require_once('../models/main.model.php');
+
+$action = (isset($_POST['action']) ? trim($_POST['action']) : null);
+if($action == 'login')
+{
+	$result = array();
+	$result['message'] = 'Error occured please contact the system administrator.';
+	$result['error'] = true;
+	$user_id = trim($_POST['user_id']);
+	$password = $_POST['password'];
+
+	if(empty($user_id))
+	{
+		$result['message'] = 'Please fill in User ID.';
+	}
+	elseif(empty($password))
+	{
+		$result['message'] = 'Please fill in Password';
+	}
+	else
+	{
+		$login = $main->login($user_id, $password);
+		if($login)
+		{
+			$result['message'] = 'Access granted.';
+			$result['error'] = false;
+		}
+		else
+		{
+			$result['message'] = 'Access denied.';
+		}
+	}
+
+	echo json_encode($result);
+}
+
+?>
